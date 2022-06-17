dcb92d8e5d3a04c682caa39a97d7782167b6e4e2
GoodGuy4221 <75629090+GoodGuy4221@users.noreply.github.com>
2022-06-17 09:51:20 +0400

    1-application-framework

---
+++ blog_on_flask/main/routes.py
@@ -0,0 +1,46 @@
+from flask import render_template, Blueprint
+
+from blog_on_flask.models import User
+
+main = Blueprint('main', __name__)
+
+
+@main.route('/')
+@main.route('/index')
+def index():
+    context = {
+        'page_title': 'главная',
+    }
+    return render_template('index.html', context=context)
+
+
+@main.route('/examples')
+def examples():
+    context = {
+        'page_title': 'примеры',
+    }
+    return render_template('examples.html', context=context)
+
+
+@main.route('/page')
+def page():
+    context = {
+        'page_title': 'страница',
+    }
+    return render_template('page.html', context=context)
+
+
+@main.route('/another-page')
+def another_page():
+    context = {
+        'page_title': 'еще одна страница',
+    }
+    return render_template('another_page.html', context=context)
+
+
+@main.route('/contact')
+def contact():
+    context = {
+        'page_title': 'контакты',
+    }
+    return render_template('contact.html', context=context)
