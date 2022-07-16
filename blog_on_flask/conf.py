b725c042e67a9a584be2f78cbd3684207226c212
Eric <evyrfgp@gmail.com>
2022-07-16 13:55:00 +0400

    ...

--- blog_on_flask/conf.py
+++ blog_on_flask/conf.py
@@ -2,11 +2,11 @@ class Conf:
     SQLALCHEMY_DATABASE_URI = 'sqlite:///db.sqlite3'
     SECRET_KEY = 'd5065fbb34b28d1c84a9d6fde2c388e834e061628466328ba95331dbe5b5be30'
 
-    SENDER = ''
+    SENDER = 's1te.sender@yandex.ru'
     MAIL_SERVER = 'smtp.yandex.ru'
     MAIL_PORT = 465
     MAIL_USE_SSL = True
-    MAIL_USERNAME = ''
-    MAIL_PASSWORD = ''
+    MAIL_USERNAME = 's1te.sender@yandex.ru'
+    MAIL_PASSWORD = 'nvmwecrsphkqzjtc'
 
-    MY_EMAIL = ''
+    MY_EMAIL = 'evyrfgp@gmail.com'
